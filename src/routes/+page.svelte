b5112bb30e948cb5b2e8e930bb2cd0571b7466e1
drewjyaa <andre@sahamrakyat.com>
2023-10-27 15:01:53 +0700

    update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Motion Sport Indonesia</title>
+	<meta name="description" content="Home" />
+</svelte:head>
+
+<div class="grid place-content-center h-screen bg-zinc-800 text-white text-center">
+	<div class="text-3xl text-white font-black">MotionSportIndonesia</div>
+	<div class="text-2xl font-semibold italic mt-10">Coming Soon</div>
+</div>
